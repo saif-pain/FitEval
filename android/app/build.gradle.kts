901c33d22edf11058b818c7642327bbc006008a5
saif-pain <saifalmamun555@gmail.com>
2025-06-20 17:10:16 +0600

    modifying bundle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fit_eval"
+        applicationId = "com.saifpain.fit_eval"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
